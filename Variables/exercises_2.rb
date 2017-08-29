faeb2ae772a9cdf327ae5bc36fc6b2d3162fe86b
grumoz <gitgramo@gmail.com>
2017-08-29 08:53:21 +0300

    chapter Variables

---
+++ Variables/exercises_2.rb
@@ -0,0 +1,23 @@
+#4
+first = gets.chomp
+last = gets.chomp
+puts first + " " + last
+
+#3
+puts "name:"
+name = gets.chomp
+10.times do
+  puts name
+end
+
+#2
+puts "Age:"
+age = gets.chomp.to_i
+puts "In 10 yrs you will be " + (age + 10).to_s
+puts "In 20 yrs you will be " + (age + 20).to_s
+puts "In 30 yrs you will be " + (age + 30).to_s
+puts "In 40 yrs you will be " + (age + 40).to_s
+#1
+name = gets.chomp
+puts name + ", hello"
+              
